9fe1c2f20bb6b8f73173341031e350e6d2a63722
Carlos Gutierrez <caralextan@hotmail.com>
2021-12-26 12:22:45 -0500

    add songs

--- pages/api/botones.js
+++ pages/api/botones.js
@@ -1,157 +1,122 @@
 export const botones = [
   {
-    descripcion: "Caballo",
-    imagen: "/imagen/caballo.png",
-    sonido: "/sonido/caballo.mp3",
-  },
-  {
-    descripcion: "Vaca",
-    imagen: "/imagen/vaca.png",
-    sonido: "/sonido/vaca.mp3",
-  },
-  {
-    descripcion: "Avión",
+    descripcion: "avión",
     imagen: "/imagen/avion.png",
     sonido: "/sonido/avion.mp3",
-  },{
-    descripcion: "Barco",
-    imagen: "/imagen/barco.png",
-    sonido: "/sonido/barco.mp3",
-  },{
-    descripcion: "Burro",
+  },
+  {
+    descripcion: "burro",
     imagen: "/imagen/burro.png",
     sonido: "/sonido/burro.mp3",
-  },{
-    descripcion: "Campana",
-    imagen: "/imagen/campana.png",
-    sonido: "/sonido/campana.mp3",
-  },{
-    descripcion: "Carro",
-    imagen: "/imagen/carro.png",
-    sonido: "/sonido/carro.mp3",
-  },{
-    descripcion: "Casa",
+  },
+  {
+    descripcion: "casa",
     imagen: "/imagen/casa.png",
     sonido: "/sonido/casa.mp3",
-  },{
-    descripcion: "Chancho",
-    imagen: "/imagen/chancho.png",
-    sonido: "/sonido/chancho.mp3",
-  },{
+  },
+  {
+    descripcion: "dedo",
+    imagen: "/imagen/dedo.png",
+    sonido: "/sonido/dedo.mp3",
+  },
+  {
     descripcion: "elefante",
     imagen: "/imagen/elefante.png",
     sonido: "/sonido/elefante.mp3",
-  },{
-    descripcion: "Foco",
+  },
+  {
+    descripcion: "foco",
     imagen: "/imagen/focos.png",
-    sonido: "/sonido/focos.mp3",
-  },{
-    descripcion: "Gallina",
-    imagen: "/imagen/gallina.png",
-    sonido: "/sonido/gallina.mp3",
-  },{
-    descripcion: "Gato",
+    sonido: "/sonido/foco.mp3",
+  },
+  {
+    descripcion: "gato",
     imagen: "/imagen/gato.png",
     sonido: "/sonido/gato.mp3",
-  },{
-    descripcion: "Huevo",
+  },
+  {
+    descripcion: "huevo",
     imagen: "/imagen/huevo.png",
     sonido: "/sonido/huevo.mp3",
-  },{
-    descripcion: "Iguana",
+  },
+  {
+    descripcion: "iguana",
     imagen: "/imagen/iguana.png",
     sonido: "/sonido/iguana.mp3",
-  },{
-    descripcion: "Jarra",
+  },
+  {
+    descripcion: "jarra",
     imagen: "/imagen/jarra.png",
     sonido: "/sonido/jarra.mp3",
-  },{
-    descripcion: "Lana",
+  },
+  {
+    descripcion: "lana",
     imagen: "/imagen/lana.png",
     sonido: "/sonido/lana.mp3",
-  },{
-    descripcion: "Llaves",
+  },
+  {
+    descripcion: "llaves",
     imagen: "/imagen/llaves.png",
     sonido: "/sonido/llaves.mp3",
-  },{
-    descripcion: "Mamá",
+  },
+  {
+    descripcion: "mamá",
     imagen: "/imagen/mama.png",
     sonido: "/sonido/mama.mp3",
-  },{
-    descripcion: "Moto",
-    imagen: "/imagen/moto.png",
-    sonido: "/sonido/moto.mp3",
-  },{
-    descripcion: "Niño",
-    imagen: "/imagen/nino.png",
-    sonido: "/sonido/nino.mp3",
-  },{
-    descripcion: "Olla",
+  },
+  {
+    descripcion: "nene",
+    imagen: "/imagen/nene.png",
+    sonido: "/sonido/nene.mp3",
+  },
+  {
+    descripcion: "olla",
     imagen: "/imagen/olla.png",
     sonido: "/sonido/olla.mp3",
-  },{
-    descripcion: "Oveja",
-    imagen: "/imagen/oveja.png",
-    sonido: "/sonido/oveja.mp3",
-  },{
-    descripcion: "Papá",
+  },
+  {
+    descripcion: "papá",
     imagen: "/imagen/papa.png",
     sonido: "/sonido/papa.mp3",
-  },{
-    descripcion: "Pato",
-    imagen: "/imagen/pato.png",
-    sonido: "/sonido/pato.mp3",
-  },{
-    descripcion: "Perro",
-    imagen: "/imagen/perro.png",
-    sonido: "/sonido/perro.mp3",
-  },{
-    descripcion: "Pollito",
-    imagen: "/imagen/pollito.png",
-    sonido: "/sonido/pollito.mp3",
-  },{
-    descripcion: "Queso",
+  },
+  {
+    descripcion: "queso",
     imagen: "/imagen/queso.png",
     sonido: "/sonido/queso.mp3",
-  },{
-    descripcion: "Reloj",
-    imagen: "/imagen/reloj.png",
-    sonido: "/sonido/reloj.mp3",
-  },{
-    descripcion: "Rosa",
+  },
+  {
+    descripcion: "rosa",
     imagen: "/imagen/rosa.png",
     sonido: "/sonido/rosa.mp3",
-  },{
-    descripcion: "Sapo",
+  },
+  {
+    descripcion: "sapo",
     imagen: "/imagen/sapo.png",
     sonido: "/sonido/sapo.mp3",
-  },{
-    descripcion: "Señalar",
-    imagen: "/imagen/senialar.png",
-    sonido: "/sonido/senialar.mp3",
-  },{
-    descripcion: "Telefono",
-    imagen: "/imagen/telefono.png",
-    sonido: "/sonido/telefono.mp3",
-  },{
-    descripcion: "Tomate",
+  },
+  {
+    descripcion: "tomate",
     imagen: "/imagen/tomate.png",
     sonido: "/sonido/tomate.mp3",
-  },{
-    descripcion: "Tren",
-    imagen: "/imagen/tren.png",
-    sonido: "/sonido/tren.mp3",
-  },{
-    descripcion: "Uvas",
+  },
+  {
+    descripcion: "uva",
     imagen: "/imagen/uvas.png",
-    sonido: "/sonido/uvas.mp3",
-  },{
-    descripcion: "Vaca",
+    sonido: "/sonido/uva.mp3",
+  },
+  {
+    descripcion: "vaca",
     imagen: "/imagen/vaca.png",
     sonido: "/sonido/vaca.mp3",
-  },{
-    descripcion: "Zapato",
+  },
+  {
+    descripcion: "yate",
+    imagen: "/imagen/yate.png",
+    sonido: "/sonido/yate.mp3",
+  },
+  {
+    descripcion: "zapato",
     imagen: "/imagen/zapato.png",
     sonido: "/sonido/zapato.mp3",
-  }
+  },
 ];
